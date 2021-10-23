ab06cb4634d7501821c3cca4c6a75a6e36d0ddfe
kartik_sachu <sachinkartik166@gmail.com>
2021-10-23 21:32:47 +0530

    all categories done

--- app/src/main/java/com/nbird/vtusgpacalculator/MainActivity.java
+++ app/src/main/java/com/nbird/vtusgpacalculator/MainActivity.java
@@ -1,24 +1,46 @@
 package com.nbird.vtusgpacalculator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     RecyclerView recyclerView;
     List<Modes> modesList;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
+    String link="https://play.google.com/store/apps/details?id=com.nbird.vtusgpacalculator";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         recyclerView=(RecyclerView) findViewById(R.id.recyclerview);
         modesList=new ArrayList<>();
         setModesList();
@@ -26,6 +48,87 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
 
         recyclerView.setAdapter(myAdapter);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable((Color.parseColor("#98A8D0"))));
+
+        mToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.open,R.string.close);
+
+        navigationView.bringToFront();
+
+
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_rate:
+                        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                        startActivity(browserIntent);
+                        break;
+                    case R.id.share_us:
+                        Toast.makeText(MainActivity.this, "Share Me!", Toast.LENGTH_SHORT).show();
+                        Intent shareIntent=new Intent(Intent.ACTION_SEND);
+                        shareIntent.setType("text/plane");
+                        String shareBody="MindScape: The ultimate SGPA And CGPA Calculator!\n\n" +
+                                "Download Now! \n" + link;
+                        String sharesub="VTU SGPA Calculator";
+
+                        shareIntent.putExtra(Intent.EXTRA_SUBJECT,sharesub);
+                        shareIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                        startActivity(Intent.createChooser(shareIntent,"Share Using"));
+                        break;
+                    case R.id.nav_contact:
+                        Intent intent2=new Intent(Intent.ACTION_SEND);
+                        String[] recipients={"niftynile@gmail.com"};
+                        intent2.putExtra(Intent.EXTRA_EMAIL, recipients);
+                        // intent2.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                        // intent2.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                        // intent2.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                        intent2.setType("text/html");
+                        intent2.setPackage("com.google.android.gm");
+                        startActivity(Intent.createChooser(intent2, "Send mail"));
+                        break;
+                    case R.id.nav_about:
+                        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this,R.style.AlertDialogTheme);
+                        final View view1= LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_about_us,(ConstraintLayout) findViewById(R.id.layoutDialogContainer));
+                        builder.setView(view1);
+                        builder.setCancelable(true);
+
+
+                        Button doneButton=(Button) view1.findViewById(R.id.doneButton);
+
+                        final AlertDialog alertDialog=builder.create();
+                        if(alertDialog.getWindow()!=null){
+                            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                        }
+                        try{
+                            alertDialog.show();
+                        }catch (Exception e){
+
+                        }
+
+
+                        doneButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                alertDialog.dismiss();
+                            }
+                        });
+
+
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+        navigationView.setCheckedItem(R.id.nav_view);
 
     }
 
@@ -38,4 +141,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
--- app/src/main/java/com/nbird/vtusgpacalculator/RecyclerViewAdapter.java
+++ app/src/main/java/com/nbird/vtusgpacalculator/RecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.textfield.TextInputEditText;
+
 
 import java.util.List;
 
@@ -71,7 +74,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   branchAndSemeterAlertDialog(v);
+                switch (position){
+                    case 0:
+                        branchAndSemeterAlertDialog(v);break;
+
+                    case 1:
+                        cgpaCalculatorAlertDialog(v);break;
+
+                    case 2:
+                        Intent browserIntenttos = new Intent(Intent.ACTION_VIEW, Uri.parse("https://results.vtu.ac.in/JAEcbcs/index.php"));
+                        mContext.startActivity(browserIntenttos);break;
+                    case 3:
+                        percentageAlertDialog(v);break;
+
+                }
+
             }
         });
 
@@ -103,6 +120,209 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         }
 
 
+    }
+
+
+    private void percentageAlertDialog(View v){
+        AlertDialog.Builder builder=new AlertDialog.Builder(mContext,R.style.AlertDialogTheme);
+
+        final View view1= LayoutInflater.from(mContext).inflate(R.layout.dialog_percentage_input,(ConstraintLayout) v.findViewById(R.id.layoutDialogContainer));
+        builder.setView(view1);
+        builder.setCancelable(true);
+
+
+        TextInputEditText t1=(TextInputEditText) view1.findViewById(R.id.t1);
+        Button submitButton=(Button) view1.findViewById(R.id.submitButton);
+
+
+
+
+        final AlertDialog alertDialog=builder.create();
+        if(alertDialog.getWindow()!=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        try{
+            alertDialog.show();
+        }catch (Exception e){
+
+        }
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                float[] cgpa=new float[1];
+
+
+
+                if(t1.getText().toString().equals("")){
+                    t1.setError("Fields Cannot Be Empty");
+                }else if(Float.parseFloat(t1.getText().toString())>10f){
+                    t1.setError("Fields Are Invalid! Should Be Less Than Or Equala To 10");
+                } else{
+                    alertDialog.dismiss();
+                    cgpa[0]=Float.parseFloat(t1.getText().toString());
+                    resultDisplay(v,3,cgpa);
+
+                }
+
+
+
+            }
+        });
+
+
+    }
+
+    private void cgpaCalculatorAlertDialog(View v){
+        AlertDialog.Builder builder=new AlertDialog.Builder(mContext,R.style.AlertDialogTheme);
+
+        final View view1= LayoutInflater.from(mContext).inflate(R.layout.dialog_cgpa_entry,(ConstraintLayout) v.findViewById(R.id.layoutDialogContainer));
+        builder.setView(view1);
+        builder.setCancelable(true);
+
+        TextInputEditText et1=(TextInputEditText) view1.findViewById(R.id.t1);
+        TextInputEditText et2=(TextInputEditText) view1.findViewById(R.id.t2);
+        TextInputEditText et3=(TextInputEditText) view1.findViewById(R.id.t3);
+        TextInputEditText et4=(TextInputEditText) view1.findViewById(R.id.t4);
+        TextInputEditText et5=(TextInputEditText) view1.findViewById(R.id.t5);
+        TextInputEditText et6=(TextInputEditText) view1.findViewById(R.id.t6);
+        TextInputEditText et7=(TextInputEditText) view1.findViewById(R.id.t7);
+        TextInputEditText et8=(TextInputEditText) view1.findViewById(R.id.t8);
+
+        Button submitButton=(Button) view1.findViewById(R.id.submitButton);
+
+
+
+
+        final AlertDialog alertDialog=builder.create();
+        if(alertDialog.getWindow()!=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        try{
+            alertDialog.show();
+        }catch (Exception e){
+
+        }
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.cancel();
+
+                float[] arr=new float[8];
+                arr[0]=valueGetterForCGPA(et1);
+                arr[1]=valueGetterForCGPA(et2);
+                arr[2]=valueGetterForCGPA(et3);
+                arr[3]=valueGetterForCGPA(et4);
+                arr[4]=valueGetterForCGPA(et5);
+                arr[5]=valueGetterForCGPA(et6);
+                arr[6]=valueGetterForCGPA(et7);
+                arr[7]=valueGetterForCGPA(et8);
+
+                resultDisplay(v,2,arr);
+
+
+            }
+        });
+    }
+
+
+    private void resultDisplay(View v, int i, float[] arr){
+        AlertDialog.Builder builder=new AlertDialog.Builder(mContext,R.style.AlertDialogTheme);
+
+        final View view1= LayoutInflater.from(mContext).inflate(R.layout.dialog_result_display,(ConstraintLayout) v.findViewById(R.id.layoutDialogContainer));
+        builder.setView(view1);
+        builder.setCancelable(true);
+
+
+        TextView resultText=(TextView) view1.findViewById(R.id.resultText);
+        Button cancelButton=(Button) view1.findViewById(R.id.cancelButton);
+        Button reButton=(Button) view1.findViewById(R.id.reButton);
+        TextView headText=(TextView) view1.findViewById(R.id.headText);
+
+
+
+
+
+        if(i==2){
+            headText.setText("Your CGPA");
+            float sum=0;
+            int div=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]!=0){
+                    div++;
+                    sum=sum+arr[j];
+                }
+
+            }
+
+            sum=sum/div;
+            String formattedString = String.format("%.02f", sum);
+
+            resultText.setText(formattedString);
+
+        }else if(i==3){
+            headText.setText("Your Percentage");
+            float per= (float) (arr[0]*9.5);
+            String formattedString = String.format("%.02f", per);
+            resultText.setText(formattedString+"%");
+
+        }
+
+
+
+        final AlertDialog alertDialog=builder.create();
+        if(alertDialog.getWindow()!=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        try{
+            alertDialog.show();
+        }catch (Exception e){
+
+        }
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+
+        reButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                if(i==2){
+                    cgpaCalculatorAlertDialog(v);
+                }else if(i==3){
+                    percentageAlertDialog(v);
+                }
+
+            }
+        });
+
+
+    }
+
+
+    private float valueGetterForCGPA(TextInputEditText et){
+        float marks;
+
+        String m=et.getText().toString();
+
+        if(m.equals("")){
+            marks=0;
+        }else{
+            marks=Float.parseFloat(m);
+        }
+
+
+        return marks;
     }
 
    private void branchAndSemeterAlertDialog(View v){
